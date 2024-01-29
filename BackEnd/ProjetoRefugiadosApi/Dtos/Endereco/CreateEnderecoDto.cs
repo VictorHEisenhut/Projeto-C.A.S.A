054903df968947a8d4f3bb180a2b0b1d39875a24
victor <victorhe2006@gmail.com>
2024-01-29 00:41:17 -0300

    Criando model e controller de Endereco

---
+++ BackEnd/ProjetoRefugiadosApi/Dtos/Endereco/CreateEnderecoDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoRefugiadosApi.Dtos.Endereco
+{
+    public class CreateEnderecoDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string CEP { get; set; }
+
+    }
+}
---
+++ BackEnd/ProjetoRefugiadosApi/Profiles/EnderecoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjetoRefugiados.Models;
+using ProjetoRefugiadosApi.Dtos.Endereco;
+
+namespace ProjetoRefugiadosApi.Profiles
+{
+    public class EnderecoProfile : Profile
+    {
+        public EnderecoProfile()
+        {
+            CreateMap<Endereco, CreateEnderecoDto>();
+            CreateMap<CreateEnderecoDto, Endereco>();
+        }
+    }
+}
