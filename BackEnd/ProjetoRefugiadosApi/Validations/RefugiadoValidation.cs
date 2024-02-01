c18dc3a41373bc0ad9eb367f9f20cd354e386776
victor <victorhe2006@gmail.com>
2024-02-01 18:23:42 -0300

    Criando restante de validações para cadastro de refugiados

--- BackEnd/ProjetoRefugiadosApi/Validations/RefugiadoValidation.cs
+++ BackEnd/ProjetoRefugiadosApi/Validations/RefugiadoValidation.cs
@@ -1,5 +1,6 @@
 ﻿using DocumentValidator;
 using FluentValidation;
+using NuGet.Packaging;
 using ProjetoRefugiados.Models;
 
 namespace ProjetoRefugiadosApi.Validations
@@ -9,17 +10,22 @@ namespace ProjetoRefugiadosApi.Validations
         public RefugiadoValidation()
         {
             RuleFor(refugiado => refugiado.Nome).MinimumLength(3).MaximumLength(20).NotNull().WithMessage("Nome deve possuir entre 3 e 20 caracteres.");
-            RuleFor(p => p.Senha).NotEmpty().WithMessage("Sua senha não pode estar vazia.")
-                               .MinimumLength(8).WithMessage("Sua senha deve possuir ao mínimo 8 caracteres.")
-                               .MaximumLength(16).WithMessage("Sua senha não deve exceder 16 caracteres.")
-                               .Matches(@"[A-Z]+").WithMessage("Sua senha deve possuir ao menos uma letra maiúscula.")
-                               .Matches(@"[a-z]+").WithMessage("Sua senha deve possuir ao menos uma letra minúscula.")
-                               .Matches(@"[0-9]+").WithMessage("Sua senha deve possuir ao menos um número.");
+            RuleFor(p => p.Senha)
+                .NotEmpty().WithMessage("Sua senha não pode estar vazia.")
+                .MinimumLength(8).WithMessage("Sua senha deve possuir ao mínimo 8 caracteres.")
+                .MaximumLength(16).WithMessage("Sua senha não deve exceder 16 caracteres.")
+                .Matches(@"[A-Z]+").WithMessage("Sua senha deve possuir ao menos uma letra maiúscula.")
+                .Matches(@"[a-z]+").WithMessage("Sua senha deve possuir ao menos uma letra minúscula.")
+                .Matches(@"[0-9]+").WithMessage("Sua senha deve possuir ao menos um número.");
             RuleFor(refugiado => CpfValidation.Validate(refugiado.Documento.Cpf)).Equal(true).WithMessage("CPF inválido").When(r => r.Documento.Cpf != null);
-            
             //Em comentário para testes:
             //RuleFor(refugiado => RGValidation.Validate(refugiado.Documento.Rg)).Equal(true).WithMessage("RG inválido");
             //RuleFor(refugiado => CnhValidation.Validate(refugiado.Documento.Cnh)).Equal(true).WithMessage("CNH inválida");
+            RuleFor(refugiado => refugiado.Genero).NotNull().IsInEnum().WithMessage("Gênero inválido.");
+            RuleFor(refugiado => refugiado.EstadoCivil).NotNull().IsInEnum().WithMessage("Estado civil inválido.");
+            RuleFor(refugiado => refugiado.Escolaridade).NotNull().IsInEnum().WithMessage("Escolaridade inválida.");
+            RuleFor(refugiado => refugiado.DataNascimento).NotNull().LessThan(DateOnly.FromDateTime(DateTime.Now)).WithMessage("Data de nascimento inválida.");
+            RuleFor(refugiado => refugiado.Telefone).NotNull().Length(11).WithMessage("Telefone inválido, verifique se contém o DDD.");
         }
     }
 }
