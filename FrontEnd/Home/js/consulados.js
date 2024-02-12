95559e7c3def36df2b3af37f6c6c3c74d81591a4
Pedro França de Carvalho <pedro.carvalho@proway.treina>
2024-02-12 17:20:34 -0300

    Adicionando os icones nos links de abrigos, consulados e postos de saúde

--- FrontEnd/Home/js/consulados.js
+++ FrontEnd/Home/js/consulados.js
@@ -19,7 +19,14 @@ fetch('http://localhost:5145/api/Consulados',{
                 <li class="list-group-item">${element.endereco.rua}, ${element.endereco.numero}<br>${element.endereco.bairro}<br>${element.endereco.cidade} - ${element.endereco.estado}<br>${element.endereco.cep}</li>
               </ul>
               <div class="card-body">
-                  <button onclick="linkEndereco('${element.endereco.rua}','${element.endereco.numero}','${element.endereco.bairro}','${element.endereco.cidade}','${element.endereco.estado}','${element.endereco.cep}')" class="btn btn-outline-primary">Link endereço</button>
+                  <button onclick="linkEndereco('${element.endereco.rua}',
+                  '${element.endereco.numero}','${element.endereco.bairro}',
+                  '${element.endereco.cidade}','${element.endereco.estado}',
+                  '${element.endereco.cep}')" 
+                  class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="29" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg> Link endereço</button>
               </div>
               </div>`
               selectConsulado.innerHTML += consulados
