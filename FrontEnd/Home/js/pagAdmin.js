e3f8d8abbdab9cfa50142fbb7f98ee7795a5faac
pedro <pedroentra21@gmail.com>
2024-02-22 15:50:20 -0300

    Criando a tabela de refugiados da pag de admin

--- FrontEnd/Home/js/pagAdmin.js
+++ FrontEnd/Home/js/pagAdmin.js
@@ -7,50 +7,43 @@ fetch('http://localhost:5145/api/Refugiados',{
         .then(data => {return data.json();})
         .then(post => {
             post.data.forEach(element => {
-                refugiados = `<p>${element.nome}</p>`
+                refugiados = `<tr>
+                <td>${element.id}</td>
+                <td>${element.nome}</td>
+                <td>${element.sobrenome}</td>
+                <td>${element.email}</td>
+                <td>${element.telefone}</td>
+                <td>${element.pais.pais}</td>
+                <tr>
+                `
                 selectRefugiados.innerHTML += refugiados
             });
             refugiadosNumero.innerHTML =  post.total
         }); 
 
-selectAbrigos = document.getElementById("div_admin_abrigos")
 let abrigosNumero = document.querySelector("#p_abrigos_numero")
 fetch('http://localhost:5145/api/Abrigos',{
                 headers: {'Authorization': 'Bearer ' + token,'Content-Type': 'application/json'}
               })
         .then(data => {return data.json();})
         .then(post => {
-            post.data.forEach(element => {
-                abrigos = `<p>${element.nome}</p>`
-                selectAbrigos.innerHTML += abrigos
-            });
             abrigosNumero.innerHTML =  post.total
         }); 
 
-selectConsulados = document.getElementById("div_admin_consulados")
 let consuladosNumero = document.querySelector("#p_consulados_numero")
 fetch('http://localhost:5145/api/Consulados',{
                 headers: {'Authorization': 'Bearer ' + token,'Content-Type': 'application/json'}
               })
         .then(data => {return data.json();})
         .then(post => {
-            post.data.forEach(element => {
-                consulados = `<p>${element.nome}</p>`
-                selectConsulados.innerHTML += consulados
-            });
             consuladosNumero.innerHTML =  post.total
         });
 
-selectPostos = document.getElementById("div_admin_postos")
 let postosNumero = document.querySelector("#p_postos_numero")
 fetch('http://localhost:5145/api/PostoDeSaude',{
                 headers: {'Authorization': 'Bearer ' + token,'Content-Type': 'application/json'}
               })
         .then(data => {return data.json();})
         .then(post => {
-            post.data.forEach(element => {
-                postos = `<p>${element.nome}</p>`
-                selectPostos.innerHTML += postos
-            });
             postosNumero.innerHTML =  post.total
         });
