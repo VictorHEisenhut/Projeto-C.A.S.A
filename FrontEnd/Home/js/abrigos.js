691d514b16b3fddddf95dda655f5929f07b8a21e
pedro <pedroentra21@gmail.com>
2024-02-25 21:27:10 -0300

    corrigindo página de abrigos e começado a fazer deletar endereços para admis

--- FrontEnd/Home/js/abrigos.js
+++ FrontEnd/Home/js/abrigos.js
@@ -1,49 +1,124 @@
+let tokens = localStorage.getItem('token')
 selectAbrigo = document.getElementById("abrigos")
 window.addEventListener('load', function() {
   // Oculte a loading screen
   var loadingScreen = document.getElementById('loadingScreen');
-  
   fetch('http://localhost:5145/api/Abrigos',{
-    headers: { 
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(data => {return data.json();})
-  .then(post => {
-    post.forEach(element => {
-      
-      abrigos = `<div class="card border-secondary" style="width: 18rem;">
-      <div class="card-body">
-      <h5 class="card-title">Abrigo</h5>
-      <p class="card-text">${element.nome}</p>
-      </div>
-      <ul class="list-group list-group-flush">
-      <li class="list-group-item">${element.email}</li>
-      <li class="list-group-item">${element.telefone}</li>
-      <li class="list-group-item">${element.endereco.rua}, ${element.endereco.numero}<br>${element.endereco.bairro}<br>${element.endereco.cidade} - ${element.endereco.estado}<br>${element.endereco.cep}</li>
-      </ul>
-      <div class="card-body">
-      <button onclick="linkEndereco('${element.endereco.rua}',
-      '${element.endereco.numero}','${element.endereco.bairro}',
-      '${element.endereco.cidade}','${element.endereco.estado}',
-      '${element.endereco.cep}')" 
-      class="btn btn-outline-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="29" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-      <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
-      <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-    </svg> Link endereço</button>
-      </div>
-      </div>`
-      loadingScreen.style.display = 'none';
-      selectAbrigo.innerHTML += abrigos
-    });
-  });   
+                headers: { 
+                           'Content-Type': 'application/json'
+                          }
+              })
+        .then(data => {return data.json();})
+        .then(post => {
+            post.data.forEach(element => {
+                
+                abrigos = `<div class="card border-secondary card_cons">
+                <div class="card-body">
+                <h5 class="card-title">Abrigos</h5>
+                <p class="card-text">${element.nome}</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">${element.email}</li>
+                <li class="list-group-item">${element.telefone}</li>
+                <li class="list-group-item">${element.endereco.rua}, ${element.endereco.numero}<br>${element.endereco.bairro}<br>${element.endereco.cidade} - ${element.endereco.estado}<br>${element.endereco.cep}</li>
+              </ul>
+              <div class="card-body">
+                  <button onclick="linkEndereco('${element.endereco.rua}',
+                  '${element.endereco.numero}','${element.endereco.bairro}',
+                  '${element.endereco.cidade}','${element.endereco.estado}',
+                  '${element.endereco.cep}')" 
+                  class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="29" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg> Link endereço</button>
+                
+              </div>
+              </div>`
+              loadingScreen.style.display = 'none';
 
-  // Exiba o conteúdo do site
-  selectAbrigo.style.display = 'flex';
+              selectAbrigo.innerHTML += abrigos
+            });
+        });   
+
+        selectAbrigo.style.display = 'flex';
 });
 
-        function linkEndereco(rua,numero,bairro,cidade,estado,cep){
-            window.open(`https://www.google.com.br/maps/place/R.+${rua},+${numero}+${bairro},+${cidade}+${estado},+${cep}`,'_blank')
+function linkEndereco(rua,numero,bairro,cidade,estado,cep){
+    window.open(`https://www.google.com.br/maps/place/R.+${rua},+${numero}+${bairro},+${cidade}+${estado},+${cep}`,'_blank')
+}
+async function confirmaDeletar(id){
+  await fetch(`http://localhost:5145/api/Abrigos/${id}`,
+{
+    method: "DELETE",
+    headers: {'Authorization': 'Bearer ' + tokens,'Content-Type': 'application/json'},
+})
+.then(response => {
+  if (!response.ok) {
+    showModal(false)  
+        response.text().then(t => console.log(t))
+  }
+  else{
+    showModal(true)
+  }
+})
+}
+    
+async function showModal(status, id) {
+          
+    var modalObj = document.getElementById("successModal");
+    var bg = document.getElementById("modalBackground");
+    var close = document.getElementById("modalClose");
+    var btnClose = document.getElementById("btnModalClose");
+    var div = document.getElementById("divText")
+    var label = document.getElementById("successModalLabel")
+    var btnDelete = document.getElementById("btnDelete")
 
-        }
+    btnDelete.onclick = function(){
+        confirmaDeletar(id)
+    } 
+
+
+        modalObj.style.display = "block"
+
+        bg.classList.add("modal-backdrop")
+        bg.classList.add("fade")
+        bg.classList.add("show")
+
+    if(status == null){
+        label.innerHTML = "Você tem certeza?"
+        div.innerHTML = "Ao clicar em deletar, este abrigo e todos seus dados serão excluídos!"
+}  
+    else if(status == false){
+        label.innerHTML = "Erro"
+        div.innerHTML = "Falha ao deletar abrigo."
+        btnDelete.setAttribute("hidden", "hidden")
+}
+    else{
+      label.innerHTML = "Sucesso"
+      div.innerHTML = "Abrigo deletada com sucesso."
+      btnDelete.setAttribute("hidden", "hidden")
+    }
+
+    btnClose.onclick = function(){
+      modalObj.style.display = "none"
+      bg.classList.remove("modal-backdrop")
+      bg.classList.remove("fade")
+      bg.classList.remove("show")
+
+      if (status == true) {
+        window.location.reload()
+      }
+    }
+
+    close.onclick = function(){
+      modalObj.style.display = "none"
+      bg.classList.remove("modal-backdrop")
+      bg.classList.remove("fade")
+      bg.classList.remove("show")
+
+      if (status == true) {
+        window.location.reload()
+      }
+    }
+
+  }
--- FrontEnd/Home/js/postosDeSaude.js
+++ FrontEnd/Home/js/postosDeSaude.js
@@ -1,5 +1,4 @@
 selectPosto = document.getElementById("postos")
-
 window.addEventListener('load', function() {
   // Oculte a loading screen
   var loadingScreen = document.getElementById('loadingScreen');
@@ -28,7 +27,8 @@ fetch('http://localhost:5145/api/PostoDeSaude/Pages?pageNumber=0&pageSize=8',{
                   '${element.endereco.numero}','${element.endereco.bairro}',
                   '${element.endereco.cidade}','${element.endereco.estado}',
                   '${element.endereco.cep}')" 
-                  class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="29" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                  class="btn btn-outline-primary">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="29" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                   <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
                   <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                 </svg> Link endereço</button>
